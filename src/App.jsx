c0de4ce06fc90fbdc5067d99c9aac32cb04bd81a
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-17 17:02:26 -0500

    Maquetando HTML

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,39 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <div className="container"></div>
+      <div className="container">
+        <main className="card">
+          <section className="card__info">
+            <h2 className="card__info-title">Join our commmunity</h2>
+            <h3 className="card__info-subtitle">
+              30-day, hassle-free money back guarantee
+            </h3>
+            <p className="card__info-text">
+              Gain access to our full library of tutorials along with expert
+              code reviews. Perfect for any developers who are serious about
+              honing their skills.
+            </p>
+          </section>
+          <section className="card__subscription">
+            <h3 className="card__subtitle">Monthly Subscription</h3>
+            <h4 className="card__subscription-price">
+              $29 <span>per month</span>
+            </h4>
+            <p className="card__subscription-text">
+              Full access for less than $1 a day
+            </p>
+            <a href="#">Sign Up</a>
+          </section>
+          <section className="card__why">
+            <h3 className="card__subtitle">Why Us</h3>
+            <p className="card__why-text">
+              Tutorials by industry experts Peer & expert code review Coding
+              exercises Access to our GitHub repos Community forum Flashcard
+              decks New videos every week
+            </p>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
